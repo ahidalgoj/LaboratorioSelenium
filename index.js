c03f57930311bdada69e60378de53e43fa7d75b6
Andres Hidalgo <ahidalgoj@ucenfotec.ac.cr>
2022-07-29 23:23:54 -0600

    Ya hago la búsqueda sin filtros y recorro todas las páginas de resultados

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-const {Builder} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const {Builder, By, WebElement} = require('selenium-webdriver');
+const assert = require('assert');
+const firefox = require('selenium-webdriver/firefox');
 
-(async function openChromeTest() {
+(async function openFirefoxTest() {
   try {
-    let options = new chrome.Options();
+    let options = new firefox.Options();
     let driver = await new Builder()
                 .setChromeOptions(options)
-                .forBrowser('chrome')
+                .forBrowser('firefox')
                 .build();
-    await driver.get('https://www.google.com');
+
+    await driver.get('https://www.bnventadebienes.com/properties/search');
+
+    let searchButton = await driver.findElement(By.xpath("//button[@class='btn btn-primary-action']"));
+    await searchButton.click();
+
+    // let pagination = await driver.findElement(By.xpath("//ul[@class='pagination pagination-position']"));
+
+    // let paginationChildren = await driver.findElements(By.xpath("//ul[@class='pagination pagination-position']//child::li"));
+
+    // let lastPage = paginationChildren[paginationChildren.length-1];
+  
+    // let lastPageLink = await lastPage.findElement(By.css('a'));
+
+    // let lastPageRef = await lastPageLink.findElement(By.css('href')).getText();
+
+    let link = await driver.findElement(By.linkText(">")).then(
+      value => value.getAttribute("href")
+    );
+    
+    let paginas = Number.parseInt(link.substring(link.lastIndexOf("=")+1,link.length));
+
+    link = link.substring(0,link.lastIndexOf("=")+1);
+
+    for (let index = 1; index <= paginas; index++) {
+      
+      await driver.get(`${link}${index.toString()}`);
+
+    }
+
     await driver.quit();
+
   } catch (error) {
     console.log(error)
   }
